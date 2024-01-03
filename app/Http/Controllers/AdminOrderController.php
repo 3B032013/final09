5116243bb60d0d37c58d3bc012ed252d97e21b62
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-01-03 15:54:33 +0800

    後台訂單搜索 & 數據分頁

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -9,9 +9,29 @@ class AdminOrderController extends Controller
 {
     public function index(Request $request)
     {
-        $orders = Order::orderby('id','ASC')->get();
+        $perPage = $request->input('perPage', 10);
+        $orders = Order::orderby('id','ASC')->paginate($perPage);
         $data = ['orders' => $orders];
         return view('admins.orders.index',$data);
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        $perPage = $request->input('perPage', 10);
+
+        $orders = Order::whereHas('seller.user', function ($queryBuilder) use ($query) {
+            $queryBuilder->where('name', 'like', "%$query%");
+        })
+            ->orWhereHas('user', function ($queryBuilder) use ($query) {
+                $queryBuilder->where('name', 'like', "%$query%");
+            })
+            ->paginate($perPage);
+
+        return view('admins.orders.index', [
+            'orders' => $orders,
+            'query' => $query,
+        ]);
     }
 
     public function show(Order $order)
