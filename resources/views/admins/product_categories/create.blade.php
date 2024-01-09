ee505859e662245b6d0d50b0e69804111d3632f3
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-01-09 17:08:35 +0800

    商品類別

--- resources/views/admins/product_categories/create.blade.php
+++ resources/views/admins/product_categories/create.blade.php
@@ -1,19 +1,19 @@
 @extends('admins.layouts.master')
 
-@section('page-title', '新增書籍類別')
+@section('page-title', '新增商品類別')
 
 @section('page-content')
     <div class="container-fluid px-4">
-        <h1 class="mt-4">書籍類別管理</h1>
+        <h1 class="mt-4">商品類別管理</h1>
         <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item active">新增書籍類別</li>
+            <li class="breadcrumb-item active">新增商品類別</li>
         </ol>
         @include('admins.layouts.shared.errors')
         <form action="{{ route('admins.product_categories.store') }}" method="POST" role="form">
             @method('POST')
             @csrf
             <div class="form-group">
-                <label for="name" class="form-label">書籍類別名稱</label>
+                <label for="name" class="form-label">商品類別名稱</label>
                 <input id="name" name="name" type="text" class="form-control" value="{{ old('name') }}" placeholder="請輸入商品名稱">
             </div>
             <input type="hidden" name="status" value=1>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,7 @@
         <link href="{{asset('css/homepage-styles.css')}}" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <style>
             .custom-link {
                 color: black; /* 設置字體顏色為黑色 */
